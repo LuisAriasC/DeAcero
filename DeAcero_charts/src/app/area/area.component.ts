b838e9325cea2da592d0a489a8f11c1b5b94178c
LuisAriasC <luis.ac945@gmail.com>
2019-05-20 23:52:04 -0500

    graphs almost ready

--- DeAcero_charts/src/app/area/area.component.ts
+++ DeAcero_charts/src/app/area/area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GraphService } from '../services/graph.service';
 import { Chart } from 'angular-highcharts';
 
 import HighchartsMoreModule from 'highcharts/highcharts-more';
@@ -8,119 +9,258 @@ import OrganizationModule from 'highcharts/modules/organization';
 @Component({
   selector: 'app-area',
   templateUrl: './area.component.html',
-  styleUrls: ['./area.component.css']
+  styleUrls: ['./area.component.css'],
+  providers: [GraphService]
 })
 export class AreaComponent implements OnInit {
 
-  constructor() { }
+  public entryChart;
+  public chart;
+  public chart2;
+  public chart3;
+  public chart4;
+  public chart5;
+  public outhart;
+  private nfLigthTable: string;
+  private datanfLight: number[];
+  private h2Table: string;
+  private dataH2: number[];
+  private h34Table: string;
+  private dataH34: number[];
+  private h4Table: string;
+  private dataH4: number[];
+
+  constructor(
+    private graphService: GraphService
+  ) { }
 
   ngOnInit() {
+
+    const month = 1;
+    const month2 = 2;
+    const year = 2019;
+    const day = 0;
+
+    this.nfLigthTable = 'Non_Ferrous_Light';
+    this.graphService.getDataGraph( this.nfLigthTable, month, year).subscribe(
+      response => {
+        this.datanfLight = this.prepareJSON(response);
+        this.chart = this.createChart(year, day, month, this.nfLigthTable, this.datanfLight);
+      }
+    );
+
+    this.h2Table = 'Heavy_2';
+    this.graphService.getDataGraph( this.h2Table, month, year).subscribe(
+      response => {
+        this.dataH2 = this.prepareJSON(response);
+        this.chart2 = this.createChart(year, day, month, this.h2Table, this.dataH2);
+      }
+    );
+
+    this.h34Table = 'Heavy_3_4';
+    this.graphService.getDataGraph( this.h34Table, month, year).subscribe(
+      response => {
+        this.dataH34 = this.prepareJSON(response);
+        this.chart3 = this.createChart(year, day, month, this.h34Table, this.dataH34);
+      }
+    );
   }
 
+  createDonut( title: string,  y1: number, table1: string, y2: number, table2: string ) {
 
+    const chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: title
+      },
+      credits: {
+        enabled: false
+      },
+      series: [
+        {
+          name: 'Porcentaje',
+          data: [
+            {y: y1, color: 'green', name: table1},
+            {y: y2, color: 'blue', name: table2}
+          ],
+          type: 'pie'
+        }
+      ]
+    });
 
-  chart = new Chart({
-    title: {
-      text: 'Ferroso Ligero'
-    },
+    return chart;
+  }
 
-    xAxis: {
-      type: 'datetime'
-    },
+  createChart( year: number, day: number, month: number, table: string, data: number[] ) {
+    console.log(data);
+    console.log(this.prepareData(data, month));
+    const chart = new Chart({
+      title: {
+        text: table
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      yAxis: {
+        type: 'linear'
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          pointStart: Date.UTC(year, day, month),
+          pointIntervalUnit: 'day'
+        }
+      },
+      series: [
+        {
+          name: 'Toneladas',
+          data: this.prepareData(data, month),
+          type: 'column',
+          color: 'green'
+        }
+      ]
 
-    credits: {
-      enabled: false
-    },
-    plotOptions: {
-      series: {
-        pointStart: Date.UTC(2019, 0, 1),
-        pointIntervalUnit: 'day'
+    });
+
+    return chart;
+  }
+
+  createChart2( year, day, month, name, table1, data1, table2, data2 ) {
+
+    const chart = new Chart({
+      title: {
+        text: name
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          pointStart: Date.UTC(year, day, month),
+          pointIntervalUnit: 'day'
+        }
+      },
+      series: [
+        {
+          name: table1,
+          data: this.prepareData(data1, month),
+          type: 'line'
+        },
+        {
+          name: table2,
+          data: this.prepareData(data2, month),
+          type: 'line'
+        }
+      ]
+    });
+
+    return chart;
+  }
+
+  prepareJSON(response) {
+    const myJSON = JSON.stringify(response);
+    const graph = JSON.parse(myJSON);
+    const body = graph.body;
+    return body;
+  }
+
+  prepareData(body, month: number) {
+    console.log(body);
+    console.log(month);
+    const datos = 'Datos';
+    const fecha = 'Fecha';
+    const toneladas = 'Toneladas'
+    const len = body[datos].length;
+    const dat = [];
+    let dateStr: string;
+    let d: string;
+    let day: number;
+    let i: number;
+    let tons = 0;
+    let tonsStr: string;
+    let di = 1;
+
+    // Llenado de días con info de api
+    for (i = 0; i < len; i++) {
+      dateStr = body[datos][i][fecha];
+      tonsStr = body[datos][i][toneladas];
+      d = dateStr.substring(8, 10);
+      day = parseInt(d, 10);
+      tons = parseInt(tonsStr, 10);
+      while (di !== day) {
+        dat.push(0.0);
+        di++;
       }
-    },
+      dat.push(tons);
+      di++;
+    }
 
-    series: [
-      {
-        name: "Toneladas",
-        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-        type: "line"
-      }, {
-        name: "Toneladas",
-        data: [144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2],
-        type: "line"
-      }
+    let days = 0;
+    if ( month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+      days = 31;
+    } else if ( month === 4 || month === 6 || month === 9 || month === 11) {
+      days = 30;
+    } else {
+      days = 28;
+    }
 
-    ]
-
-
-  });
-
-  chart2 = new Chart({
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: 'Ferroso Medio'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [
-      {
-        name: 'Toneladas',
-        data: [{ y: 2000, x: 1, color: 'green' },
-        { y: 8000, x: 2, color: 'blue' },
-        { y: 10000, x: 3, color: 'red' },
-        { y: 4000, x: 4, color: 'orange' },
-        { y: 6000, x: 5, color: 'brown' }],
-        type: "bar"
-      }
-    ]
-  });
-
-  chart3 = new Chart({
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: 'Ferroso Pesado'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [
-      {
-        name: 'Toneladas',
-        data: [{ y: 2752, x: 1, color: 'black' },
-        { y: 5369, x: 2, color: 'yellow' },
-        { y: 6245, x: 3, color: 'red' },
-        { y: 7510, x: 4, color: 'blue' },
-        { y: 3500, x: 5, color: 'pink' }],
-        type: "column"
-      }
-    ]
-  });
-
-  chart4 = new Chart({
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: 'Otro'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [
-      {
-        name: 'Toneladas',
-        data: [{ y: 2000, x: 1, color: 'green' },
-        { y: 8000, x: 2, color: 'blue' },
-        { y: 10000, x: 3, color: 'red' },
-        { y: 4000, x: 4, color: 'orange' },
-        { y: 6000, x: 5, color: 'brown' }],
-        type: "spline"
-      }
-    ]
-  });
-
+    for (i = di - 1; i < days; i++) {
+      dat.push(0.0);
+    }
+    return dat;
+  }
+//
+//  chart3 = new Chart({
+//    chart: {
+//      type: 'line'
+//    },
+//    title: {
+//      text: 'Ferroso Pesado'
+//    },
+//    credits: {
+//      enabled: false
+//    },
+//    series: [
+//      {
+//        name: 'Toneladas',
+//        data: [{ y: 2752, x: 1, color: 'black' },
+//        { y: 5369, x: 2, color: 'yellow' },
+//        { y: 6245, x: 3, color: 'red' },
+//        { y: 7510, x: 4, color: 'blue' },
+//        { y: 3500, x: 5, color: 'pink' }],
+//        type: "column"
+//      }
+//    ]
+//  });
+//
+//  chart4 = new Chart({
+//    chart: {
+//      type: 'line'
+//    },
+//    title: {
+//      text: 'Otro'
+//    },
+//    credits: {
+//      enabled: false
+//    },
+//    series: [
+//      {
+//        name: 'Toneladas',
+//        data: [{ y: 2000, x: 1, color: 'green' },
+//        { y: 8000, x: 2, color: 'blue' },
+//        { y: 10000, x: 3, color: 'red' },
+//        { y: 4000, x: 4, color: 'orange' },
+//        { y: 6000, x: 5, color: 'brown' }],
+//        type: "spline"
+//      }
+//    ]
+//  });
+//
 }
--- DeAcero_charts/src/app/global/global.component.ts
+++ DeAcero_charts/src/app/global/global.component.ts
@@ -5,6 +5,7 @@ import { Chart } from 'angular-highcharts';
 import HighchartsMoreModule from 'highcharts/highcharts-more';
 import NetworkgraphModule from 'highcharts/modules/networkgraph';
 import OrganizationModule from 'highcharts/modules/organization';
+//import { create } from 'domain';
 
 @Component({
   selector: 'app-global',
@@ -16,7 +17,15 @@ export class GlobalComponent implements OnInit {
 
   public entryChart;
   public chart;
+  public chart2;
+  public chart3;
   public outhart;
+  private entryTable: string;
+  private principalTable: string;
+  private FTTable: string;
+  private dataEntry: number[];
+  private dataPrincipal: number[];
+  private dataFT: number[];
 
   constructor(
     private graphService: GraphService
@@ -24,70 +33,178 @@ export class GlobalComponent implements OnInit {
 
   ngOnInit() {
 
-    this.graphService.getDataGraph('Entrada', 1, 2018).subscribe(
+    const month = 1;
+    const month2 = 2;
+    const year = 2018;
+    const day = 0;
+
+    this.entryTable = 'Entrada';
+    // Crea grafico para entradas 
+    this.graphService.getDataGraph( this.entryTable, month, year).subscribe(
       response => {
-        const myJSON = JSON.stringify(response);
-        const graph = JSON.parse(myJSON);
-        let body = graph.body;
-        console.log(body);
-        let len = body['Datos'].length;
-
-        let dat = [];
-        let dateStr: string;
-        let d: string;
-        let day: number;
-        let i: number;
-        let tons = 0;
-        let tonsStr;
-        let di = 1;
-
-        // Llenado de días con info de api
-        for(i = 0; i < len; i++){
-          dateStr = body['Datos'][i]['Fecha'];
-          tonsStr = body['Datos'][i]['Toneladas'];
-          d = dateStr.substring(8, 10);
-          day = parseInt(d, 10);
-          tons = parseInt(tonsStr, 10);
-          while (di !== day) {
-            dat.push(0.0);
-            di++;
-          }
-          dat.push(tons);
-          di++;
-        }
-
-        let t_days = 31;
-        for(i = di - 1; i < t_days; i++) {
-          dat.push(0.0);
-        }
-
-        this.chart = new Chart({
-          title: {
-            text: 'Entradas'
-          },
-          xAxis: {
-            type: 'datetime'
-          },
-          credits: {
-            enabled: false
-          },
-          plotOptions: {
-            series: {
-              pointStart: Date.UTC(2018, 0, 1),
-              pointIntervalUnit: 'day'
-            }
-          },
-          series: [
-            {
-              name: 'Toneladas',
-              data: dat,
-              type: 'column',
-              color: 'green'
-            }
-          ]
-        });
+        this.dataEntry = this.prepareJSON(response);
+        this.chart = this.createChart(year, day, month, this.entryTable, this.dataEntry);
       }
     );
+
+    this.principalTable = 'Principal';
+    // Crea grafico para salida 1 
+    this.graphService.getDataGraph( this.principalTable, month, year).subscribe(
+      response => {
+        this.dataPrincipal = this.prepareJSON(response);
+        this.FTTable = 'Ferrous_Tramp';
+        // Crea grafico para salida 2
+        this.graphService.getDataGraph( this.FTTable, month2, year).subscribe(
+          response2 => {
+            this.dataFT = this.prepareJSON(response2);
+            this.chart2 = this.createChart2(year, day, month, 'Salidas', this.principalTable, this.dataPrincipal, this.FTTable, this.dataFT);
+            //this.chart3 = this.createDonut('Salidas', 90, this.principalTable, 10, this.FTTable);
+          }
+        );
+      }
+    );
+  }
+
+  createDonut( title, y1, table1 ,y2, table2 ){
+
+    let chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: title
+      },
+      credits: {
+        enabled: false
+      },
+      series: [
+        {
+          name: 'Porcentaje',
+          data: [
+            {y: y1, color: 'green', name: table1},
+            {y: y2, color: 'blue', name: table2}
+          ],
+          type: 'pie'
+        }
+      ]
+    });
+
+    return chart;
+  }
+
+  createChart( year, day, month, table, data_ ) {
+    let chart = new Chart({
+      title: {
+        text: table
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      yAxis: {
+        type: 'linear'
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          pointStart: Date.UTC(year, day, month),
+          pointIntervalUnit: 'day'
+        }
+      },
+      series: [
+        {
+          name: 'Toneladas',
+          data: this.prepareData(data_, month),
+          type: 'column',
+          color: 'green'
+        }
+      ]
+    });
+
+    return chart;
+  }
+
+  createChart2( year, day, month, name, table1, data1, table2, data2 ) {
+
+    let chart = new Chart({
+      title: {
+        text: name
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          pointStart: Date.UTC(year, day, month),
+          pointIntervalUnit: 'day'
+        }
+      },
+      series: [
+        {
+          name: table1,
+          data: this.prepareData(data1, month),
+          type: 'line'
+        },
+        {
+          name: table2,
+          data: this.prepareData(data2, month),
+          type: 'line'
+        }
+      ]
+    });
+    return chart;
+  }
+
+  prepareJSON(response) {
+    const myJSON = JSON.stringify(response);
+    const graph = JSON.parse(myJSON);
+    let body = graph.body;
+    return body;
+  }
+
+  prepareData(body, month) {
+    let len = body['Datos'].length;
+    let dat = [];
+    let dateStr: string;
+    let d: string;
+    let day: number;
+    let i: number;
+    let tons = 0;
+    let tonsStr: string;
+    let di = 1;
+
+    // Llenado de días con info de api
+    for(i = 0; i < len; i++) {
+      dateStr = body['Datos'][i]['Fecha'];
+      tonsStr = body['Datos'][i]['Toneladas'];
+      d = dateStr.substring(8, 10);
+      day = parseInt(d, 10);
+      tons = parseInt(tonsStr, 10);
+      while (di !== day) {
+        dat.push(0.0);
+        di++;
+      }
+      dat.push(tons);
+      di++;
+    }
+
+    let days = 0;
+    if ( month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+      days = 31;
+    } else if ( month === 4 || month === 6 || month === 9 || month === 11) {
+      days = 30;
+    } else {
+      days = 28;
+    }
+
+    for(i = di - 1; i < days; i++) {
+      dat.push(0.0);
+    }
+    return dat;
   }
 
 }
